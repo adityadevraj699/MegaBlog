cab6447e1d5e6926056d5dd0bbeb1ad2b5c88289
Aditya kumar <aditya.kumar1.cs.2022@mitmeerut.ac.in>
2024-11-10 23:35:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log("sad")
+  
+  return (
+    <>
+      <h1 className='text-blue-300 text-3xl text-center mt-2 py-2 bg-green-700'>Mega Blog </h1>
+    </>
+  )
+}
+
+export default App
